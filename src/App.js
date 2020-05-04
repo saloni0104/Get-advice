a0cbe03031f1b9a29f9c762b8aba698f0dff1ff8
Saloni <suparekh01@gmail.com>
2020-05-04 21:58:31 +0530

    Data fetched from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import axios from 'axios';
 
-import './App/css';
+import './App.css';
 
 class App extends React.Component {
 
     state = {advice: ''};
 
     componentDidMount() {
-        console.log('Component Did Mount');
+       this.fetchAdvice();
+
+    }
+
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     render() {
@@ -19,3 +30,7 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+//Using advice slip jason api
