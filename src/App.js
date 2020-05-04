fa7374d3c306eab0326cd132a0a3b0c9152b78c7
Saloni <suparekh01@gmail.com>
2020-05-04 21:31:57 +0530

    Added axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 import './App/css';
 
 class App extends React.Component {
+
+    state = {advice: ''};
+
+    componentDidMount() {
+        console.log('Component Did Mount');
+    }
+
     render() {
         return(
             <h1>APP</h1>
