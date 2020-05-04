c8b973948f3411c453d40c24eece8f28e13a65af
Saloni <suparekh01@gmail.com>
2020-05-04 22:09:36 +0530

    App structured

--- src/App.js
+++ src/App.js
@@ -31,7 +31,12 @@ class App extends React.Component {
         const {advice} = this.state;
 
         return(
-            <h1>{advice}</h1>
+            <div className="app">
+                <div className="card">
+                    <h1 className="header">{advice}</h1>
+                </div>
+            </div>
+            
             //instead of
             //<h1>{this.state.advice}</h1>
         );
